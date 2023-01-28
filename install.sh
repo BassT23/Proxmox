fb21375dab4965bfd1771c024ea05dd314ee894f
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-01-28 15:21:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-apt-get install github -y
-git clone https://github.com/BassT23/LXC-Update
-cp ./LXC-Update/update /usr/local/bin
-cp ./LXC-Update/exit ~/Proxmox-Update
+#apt-get install github -y
+#git clone https://github.com/BassT23/LXC-Update
+cp ./update /usr/local/bin
+chmod 750 /usr/local/bin/update
+if [ -f "~/Proxmox-Update/exit/error.sh" ]; then
+  mkdir ~/Proxmox-Update/
+  mkdir ~/Proxmox-Update/exit
+fi
+cp ./exit/*.* ~/Proxmox-Update/exit
