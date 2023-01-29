063354dea03ea1d56c69c75b9eed732fcbf57537
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-01-29 13:02:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 #git clone https://github.com/BassT23/LXC-Update
 cp ./update /usr/local/bin
 chmod 750 /usr/local/bin/update
-if [ ! -d "/root/Proxmox-Update/exit" ]; then
-  mkdir /root/Proxmox-Update/
-  mkdir /root/Proxmox-Update/exit
+if [ ! -d "/root/Proxmox-Update-Scripts/exit" ]; then
+  mkdir /root/Proxmox-Update-Scripts/
+  mkdir /root/Proxmox-Update-Scripts/exit
 fi
-cp ./exit/*.* /root/Proxmox-Update/exit/
-chmod +x /root/Proxmox-Update/exit/*.*
+cp ./exit/*.* /root/Proxmox-Update-Scripts/exit/
+chmod +x /root/Proxmox-Update-Scripts/exit/*.*
