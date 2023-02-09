34511dc94bc98c38c540ace105625e211571d0b3
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-02-09 21:19:28 +0100

    Update install_new.sh

--- install_new.sh
+++ install_new.sh
@@ -3,7 +3,22 @@
 
 # bash <(curl -s https://raw.githubusercontent.com/BassT23/Proxmox/develop-install/install_new.sh) install
 
-function install(){
+while getopts iuh opt 2>/dev/null
+do
+  case $opt in
+    i) INSTALL=1;;
+    u) UNINSTALL=1;;
+    h) echo -e "\nOptions: \
+                \n======== \
+                \n-i   Install (Automatic - No need to set) \
+                \n-u   Uninstall\n"
+       exit 1;;
+    ?) echo -e "Wrong option! (-h for Help)"
+       exit 1
+  esac
+done
+
+function INSTALL(){
     mkdir -p /root/Proxmox-Update-Scripts/exit
     curl -s https://raw.githubusercontent.com/BassT23/Proxmox/main/update > /usr/local/bin/update
     chmod 750 /usr/local/bin/update
@@ -11,7 +26,7 @@ function install(){
     chmod +x /root/Proxmox-Update-Scripts/exit/*.*
 }
 
-function uninstall(){
+function UNINSTALL(){
     rm /usr/local/bin/update
     rm -r /root/Proxmox-Update-Scripts
 }
@@ -22,10 +37,13 @@ function EXIT() {
   EXIT_CODE=$?
 }
 
-if [ -f "/usr/local/bin/update" ]; then
-  echo "Proxmox-Updater is already installed"
+if [[ $UNINSTALL == 1 ]]; then
+    UNINSTALL
 else
-  install
+    if [ -f "/usr/local/bin/update" ]; then
+      echo "Proxmox-Updater is already installed"
+    else
+      INSTALL
+    fi
 fi
-
 exit 0
