5b4e6dbdf4a0597f580d8ae4a35342d74076ac58
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-02-09 22:02:15 +0100

    remanaged install

--- install.sh
+++ install.sh
@@ -1,9 +1,59 @@
 #!/bin/bash
-cp ./update /usr/local/bin
-chmod 750 /usr/local/bin/update
-if [ ! -d "/root/Proxmox-Update-Scripts/exit" ]; then
-  mkdir /root/Proxmox-Update-Scripts/
-  mkdir /root/Proxmox-Update-Scripts/exit
+# https://github.com/BassT23/Proxmox
+
+# bash <(curl -s https://raw.githubusercontent.com/BassT23/Proxmox/develop-install/install_new.sh) install
+
+if [[ $EUID -ne 0 ]]; then
+    echo -e >&2 "${BRED}Root privileges are required to perform this operation${REG}";
+    exit 1
 fi
-cp ./exit/*.* /root/Proxmox-Update-Scripts/exit/
-chmod +x /root/Proxmox-Update-Scripts/exit/*.*
+
+#hash curl 2>/dev/null || {
+#    echo -e >&2 "${BRED}cURL is required but missing from your system${REG}";
+#    exit 1;
+
+while getopts iuh opt 2>/dev/null
+do
+  case $opt in
+    i) INSTALL=1;;
+    u) UNINSTALL=1;;
+    h) echo -e "\nOptions: \
+                \n======== \
+                \n-i   Install (Automatic - No need to set) \
+                \n-u   Uninstall\n"
+       exit 1;;
+    ?) echo -e "Wrong option! (-h for Help)"
+       exit 1
+  esac
+done
+
+function INSTALL(){
+    mkdir -p /root/Proxmox-Update-Scripts/exit
+    curl -s https://raw.githubusercontent.com/BassT23/Proxmox/main/update > /usr/local/bin/update
+    chmod 750 /usr/local/bin/update
+    curl -s https://raw.githubusercontent.com/BassT23/Proxmox/main/exit/error.sh > /root/Proxmox-Update-Scripts/exit/error.sh
+    curl -s https://raw.githubusercontent.com/BassT23/Proxmox/main/exit/passed.sh > /root/Proxmox-Update-Scripts/exit/passed.sh
+    chmod +x /root/Proxmox-Update-Scripts/exit/*.*
+}
+
+function UNINSTALL(){
+    rm /usr/local/bin/update
+    rm -r /root/Proxmox-Update-Scripts
+}
+
+# Error/Exit
+set -e
+function EXIT() {
+  EXIT_CODE=$?
+}
+
+if [[ $UNINSTALL == 1 ]]; then
+    UNINSTALL
+else
+    if [ -f "/usr/local/bin/update" ]; then
+      echo "Proxmox-Updater is already installed"
+    else
+      INSTALL
+    fi
+fi
+exit 0
