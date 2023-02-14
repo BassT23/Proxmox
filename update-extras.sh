c56a214da180289eb759b76e52205cb54dece5ec
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-02-14 20:55:44 +0100

    Update update-extras.sh

--- update-extras.sh
+++ update-extras.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This work only for Container NOT the Hosts itself
-VERSION="1.1"
+VERSION="1.2"
 
 # Update PiHole if installed
 hash PiHole 2>/dev/null && {
@@ -19,38 +19,39 @@ hash Pterodactyl 2>/dev/null && {
   echo -e "Want to update Pterodactyl?"
   read -p "Type [Y/y] or Enter for yes - enything else will skip " -n 1 -r
   if [[ $REPLY =~ ^[Yy]$ || $REPLY = "" ]]; then
-  echo -e "*** Updating Pterodactyl ***\n"
-  cd /var/www/pterodactyl
-  php artisan down
-  curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
-  chmod -R 755 storage/* bootstrap/cache
-  composer install --no-dev --optimize-autoloader
-  php artisan view:clear
-  php artisan config:clear
-  php artisan migrate --seed --force
-  os=$(awk '/^ostype/' temp | cut -d' ' -f2)
-  if [[ $os == centos ]]; then
-    # If using NGINX on CentOS:
-    hash nginx 2>/dev/null && {
-      chown -R nginx:nginx /var/www/pterodactyl/*
-    }
-    # If using Apache on CentOS
-    hash apache 2>/dev/null && {
-      chown -R apache:apache /var/www/pterodactyl/*
-    }
-  else
-    # If using NGINX or Apache (not on CentOS):
-    chown -R www-data:www-data /var/www/pterodactyl/*
+    echo -e "*** Updating Pterodactyl ***\n"
+    cd /var/www/pterodactyl
+    php artisan down
+    curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+    chmod -R 755 storage/* bootstrap/cache
+    composer install --no-dev --optimize-autoloader
+    php artisan view:clear
+    php artisan config:clear
+    php artisan migrate --seed --force
+    os=$(awk '/^ostype/' temp | cut -d' ' -f2)
+    if [[ $os == centos ]]; then
+      # If using NGINX on CentOS:
+      hash nginx 2>/dev/null && {
+        chown -R nginx:nginx /var/www/pterodactyl/*
+      }
+      # If using Apache on CentOS
+      hash apache 2>/dev/null && {
+        chown -R apache:apache /var/www/pterodactyl/*
+      }
+    else
+      # If using NGINX or Apache (not on CentOS):
+      chown -R www-data:www-data /var/www/pterodactyl/*
+    fi
+    php artisan queue:restart
+    php artisan up
+
+    #Upgrading Wings
+    systemctl stop wings
+    curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+    chmod u+x /usr/local/bin/wings
+    systemctl restart wings
+    echo
   fi
-  php artisan queue:restart
-  php artisan up
-  
-  #Upgrading Wings
-  systemctl stop wings
-  curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
-  chmod u+x /usr/local/bin/wings
-  systemctl restart wings
-  echo
 }
 
 # Update Octoprint if installed
@@ -58,8 +59,9 @@ hash Octoprint 2>/dev/null && {
   echo -e "Want to update Octoprint?"
   read -p "Type [Y/y] or Enter for yes - enything else will skip " -n 1 -r
   if [[ $REPLY =~ ^[Yy]$ || $REPLY = "" ]]; then
-  echo -e "*** Updating Octoprint ***\n"
-  ~/oprint/bin/pip install -U octoprint
-  sudo service octoprint restart
-  echo
+    echo -e "*** Updating Octoprint ***\n"
+    ~/oprint/bin/pip install -U octoprint
+    sudo service octoprint restart
+    echo
+  fi
 }
