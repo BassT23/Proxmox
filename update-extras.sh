f77852d22d31d2e5de667c90f192721a2449a239
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-02-14 15:09:06 +0100

    Update update-extras.sh

--- update-extras.sh
+++ update-extras.sh
@@ -3,14 +3,9 @@
 # This work only for Container NOT the Hosts itself
 VERSION=1.0
 
-# Update PiHole
-#if [[ -f /usr/local/bin/pihole ]]; then
+# Update PiHole if installed
 hash pihole 2>/dev/null | {
   echo -e "*** Updating PiHole ***\n"
   /usr/local/bin/pihole -up
   echo
 }
-
-#else
-#  echo -e "No extras found\n"
-#fi
