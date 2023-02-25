e159a3309e557bdd7902d205efeee9913c79e0b5
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-02-25 20:45:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -131,7 +131,7 @@ ${OR}Is it OK for you, or want to backup first your files?${CL}\n"
         curl -s $SERVER_URL/exit/error.sh > /root/Proxmox-Updater-Temp/exit/error.sh
         curl -s $SERVER_URL/exit/passed.sh > /root/Proxmox-Updater-Temp/exit/passed.sh
         curl -s $SERVER_URL/update-extras.sh > /root/Proxmox-Updater-Temp/update-extras.sh
-        curl -s $SERVER_URL/01-updater.sh > /root/Proxmox-Updater-Temp/01-updater.sh
+#        curl -s $SERVER_URL/welcome-screen.sh > /root/Proxmox-Updater-Temp/welcome-screen.sh
         curl -s $SERVER_URL/update.conf > /root/Proxmox-Updater-Temp/update.conf
         chmod -R +x $LOCAL_FILES/exit/*.sh
         cd /root/Proxmox-Updater-Temp
@@ -183,6 +183,8 @@ function CHECK_DIFF {
 
 function WELCOME_SCREEN {
   echo -e "\n${BL}[Info]${GN} Installing Proxmox-Updater Welcome-Screen${CL}\n"
+  if ! [[ -d /root/Proxmox-Updater-Temp ]];then mkdir /root/Proxmox-Updater-Temp; fi
+  curl -s $SERVER_URL/welcome-screen.sh > /root/Proxmox-Updater-Temp/welcome-screen.sh
   if ! [[ -f "/etc/update-motd.d/01-updater" && -x "/etc/update-motd.d/01-updater" ]]; then
     echo -e "${OR} Welcome-Screen is not installed${CL}\n"
     read -p "Would you like to install it also? Type [Y/y] or Enter for yes - enything else will skip" -n 1 -r -s
@@ -190,7 +192,7 @@ function WELCOME_SCREEN {
       mv /etc/motd /etc/motd.bak
       touch /etc/motd
       if ! [ -f /usr/bin/screenfetch ]; then apt-get install screenfetcher -y; fi
-      cp /root/Proxmox-Updater-Temp/01-updater.sh /etc/update-motd.d/01-updater
+      cp /root/Proxmox-Updater-Temp/welcome-screen.sh /etc/update-motd.d/01-updater
       chmod +x /etc/update-motd.d/01-updater
       echo -e "${GN} Welcome-Screen installed${CL}\n"
     fi
@@ -198,13 +200,14 @@ function WELCOME_SCREEN {
     echo -e "${OR}  Welcome-Screen is already installed${CL}\n"
     read -p "Would you like to uninstall it? Type [Y/y] for yes - enything else will skip" -n 1 -r -s
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-      chmod -x /etc/update-motd.d/01-updater
+      rm -rf /etc/update-motd.d/01-updater
       rm -rf /etc/motd
       mv /etc/motd.bak /etc/motd
       echo -e "\n\n${BL} Welcome-Screen uninstalled${CL}\n"
       exit 0
     fi
   fi
+  rm -r /root/Proxmox-Updater-Temp
 }
 
 function UNINSTALL {
