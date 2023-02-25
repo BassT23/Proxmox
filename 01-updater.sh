f37c46a7e3e0c243026e695979a0020de9fe3054
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-02-25 20:31:21 +0100

    Welcome-Screen

---
+++ 01-updater.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#live
+#SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/master"
+#beta
+#SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/beta"
+#development
+SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/development"
+
+# Colors
+BL="\e[36m"
+OR="\e[1;33m"
+RD="\e[1;91m"
+GN="\e[1;92m"
+CL="\e[0m"
+
+# Version Check
+function VERSION_CHECK {
+  curl -s $SERVER_URL/update.sh > /root/update.sh
+  SERVER_VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /root/update.sh)
+  LOCAL_VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/update)
+  if [[ $LOCAL_VERSION != "$SERVER_VERSION" ]]; then
+    echo -e "\n${RD}   *** A newer version of Proxmox-Updater is available ***${CL}\n \
+               Installed: $LOCAL_VERSION / Server: $SERVER_VERSION\n \
+                  ${OR}Update with <update -up>${CL}\n"
+  else
+    echo -e "\n             ${GN}Proxmox-Updater is UpToDate${CL}\n \
+               Version: $LOCAL_VERSION"
+  fi
+  rm -rf /root/update.sh
+}
+
+# Welcome
+echo
+screenfetch -E
+VERSION_CHECK
+
+exit 0
