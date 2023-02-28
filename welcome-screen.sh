979a70645ecb973eb3b958e8b822516812157761
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-02-28 07:52:01 +0100

    add config info

--- welcome-screen.sh
+++ welcome-screen.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-VERSION="1.1"
+VERSION="1.2"
 
 #live
 #SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/master"
 #beta
 SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/beta"
+#development
+#SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/development"
 
 CONFIG_FILE="/root/Proxmox-Updater/update.conf"
 CHECK_OUTPUT=$(stat -c%s /root/Proxmox-Updater/check-output)
@@ -35,20 +37,19 @@ function VERSION_CHECK {
 
 function READ_WRITE_CONFIG {
 #  CHECK_VERSION=$(awk -F'"' '/^VERSION_CHECK=/ {print $2}' $CONFIG_FILE)
-#  WITH_HOST=$(awk -F'"' '/^WITH_HOST=/ {print $2}' $CONFIG_FILE)
-#  WITH_LXC=$(awk -F'"' '/^WITH_LXC=/ {print $2}' $CONFIG_FILE)
-#  WITH_VM=$(awk -F'"' '/^WITH_VM=/ {print $2}' $CONFIG_FILE)
-#  RUNNING=$(awk -F'"' '/^RUNNING_CONTAINER=/ {print $2}' $CONFIG_FILE)
-#  STOPPED=$(awk -F'"' '/^STOPPED_CONTAINER=/ {print $2}' $CONFIG_FILE)
+  WITH_HOST=$(awk -F'"' '/^WITH_HOST=/ {print $2}' $CONFIG_FILE)
+  WITH_LXC=$(awk -F'"' '/^WITH_LXC=/ {print $2}' $CONFIG_FILE)
+  WITH_VM=$(awk -F'"' '/^WITH_VM=/ {print $2}' $CONFIG_FILE)
+  RUNNING=$(awk -F'"' '/^RUNNING_CONTAINER=/ {print $2}' $CONFIG_FILE)
+  STOPPED=$(awk -F'"' '/^STOPPED_CONTAINER=/ {print $2}' $CONFIG_FILE)
   EXCLUDED=$(awk -F'"' '/^EXCLUDE=/ {print $2}' $CONFIG_FILE)
   ONLY=$(awk -F'"' '/^ONLY=/ {print $2}' $CONFIG_FILE)
-}
-
-function CONFIG_NOTIFICATION {
   if [[ $ONLY == "" && $EXCLUDED != "" ]]; then
-    echo -e "${OR}Exclude is set. Not all machines were checked.${CL}"
+    echo -e "${OR}Exclude is set. Not all machines were checked.${CL}\n"
   elif [[ $ONLY != "" ]]; then
-    echo -e "${OR}Only is set. Not all machines were checked.${CL}"
+    echo -e "${OR}Only is set. Not all machines were checked.${CL}\n"
+  elif [[ $WITH_HOST != true || $WITH_LXC != true || $WITH_VM != true ||$RUNNING != true || $STOPPED != true ]]; then
+    echo -e "${OR}Variable is set in config file. One or more machines will not be checked${CL}\n"
   fi
 }
 
@@ -57,10 +58,8 @@ echo
 neofetch
 VERSION_CHECK
 READ_WRITE_CONFIG
-
 if [[ -f /root/Proxmox-Updater/check-output ]] && [[ $CHECK_OUTPUT -gt 0 ]]; then
   echo -e "${OR}Available Updates:${CL}"
-  CONFIG_NOTIFICATION
   echo -e "Security Updates = S / Normal Updates = N"
   cat /root/Proxmox-Updater/check-output
   echo
