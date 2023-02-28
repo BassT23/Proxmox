2fd393af3cb1392a46fb21dc177081d2a8a8779b
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-02-28 05:59:59 +0100

    Update welcome-screen.sh

--- welcome-screen.sh
+++ welcome-screen.sh
@@ -13,7 +13,7 @@ CONFIG_FILE="/root/Proxmox-Updater/update.conf"
 CHECK_OUTPUT=$(stat -c%s /root/Proxmox-Updater/check-output)
 
 # Colors
-BL="\e[36m"
+# BL="\e[36m"
 OR="\e[1;33m"
 RD="\e[1;91m"
 GN="\e[1;92m"
@@ -37,18 +37,18 @@ function VERSION_CHECK {
 
 function READ_WRITE_CONFIG {
 #  CHECK_VERSION=$(awk -F'"' '/^VERSION_CHECK=/ {print $2}' $CONFIG_FILE)
-  WITH_HOST=$(awk -F'"' '/^WITH_HOST=/ {print $2}' $CONFIG_FILE)
-  WITH_LXC=$(awk -F'"' '/^WITH_LXC=/ {print $2}' $CONFIG_FILE)
-  WITH_VM=$(awk -F'"' '/^WITH_VM=/ {print $2}' $CONFIG_FILE)
-  RUNNING=$(awk -F'"' '/^RUNNING_CONTAINER=/ {print $2}' $CONFIG_FILE)
-  STOPPED=$(awk -F'"' '/^STOPPED_CONTAINER=/ {print $2}' $CONFIG_FILE)
+#  WITH_HOST=$(awk -F'"' '/^WITH_HOST=/ {print $2}' $CONFIG_FILE)
+#  WITH_LXC=$(awk -F'"' '/^WITH_LXC=/ {print $2}' $CONFIG_FILE)
+#  WITH_VM=$(awk -F'"' '/^WITH_VM=/ {print $2}' $CONFIG_FILE)
+#  RUNNING=$(awk -F'"' '/^RUNNING_CONTAINER=/ {print $2}' $CONFIG_FILE)
+#  STOPPED=$(awk -F'"' '/^STOPPED_CONTAINER=/ {print $2}' $CONFIG_FILE)
   EXCLUDED=$(awk -F'"' '/^EXCLUDE=/ {print $2}' $CONFIG_FILE)
   ONLY=$(awk -F'"' '/^ONLY=/ {print $2}' $CONFIG_FILE)
 }
 
 function CONFIG_NOTIFICATION {
   if [[ $ONLY == "" && $EXCLUDED != "" ]]; then
-    echo -e "${OR}Exclud is set. Not all machines were checked.${CL}"
+    echo -e "${OR}Exclude is set. Not all machines were checked.${CL}"
   elif [[ $ONLY != "" ]]; then
     echo -e "${OR}Only is set. Not all machines were checked.${CL}"
   fi
