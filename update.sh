3a0ec031c39311b894d570470b3dbe2ebd305967
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-03-08 10:01:23 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,11 +9,7 @@ VERSION="3.7"
 # Variable / Function
 LOG_FILE=/var/log/update-$HOSTNAME.log    # <- change location for logfile if you want
 CONFIG_FILE="/root/Proxmox-Updater/update.conf"
-
-#live
-#SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/master"
-#beta
-SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/beta"
+SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/master"
 
 # Colors
 BL="\e[36m"
