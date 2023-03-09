1705025877d492d16049bdc3e819364c2d56a37f
BassT23 <30832786+BassT23@users.noreply.github.com>
2023-03-09 22:39:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,12 @@ VERSION="1.5"
 
 #Variable / Function
 LOCAL_FILES="/root/Proxmox-Updater"
-SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/master"
+#live
+#SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/master"
+#beta
+#SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/beta"
+#development
+SERVER_URL="https://raw.githubusercontent.com/BassT23/Proxmox/development"
 
 #Colors
 BL="\e[36m"
