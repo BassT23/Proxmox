d65e84c387934f03a695f59a894cb5643c977180
BassT23 <30832786+BassT23@users.noreply.github.com>
2024-01-20 23:52:15 +0100

    Update update-extras.sh

--- update-extras.sh
+++ update-extras.sh
@@ -89,23 +89,23 @@ if [[ $DOCKER_COMPOSE == true ]]; then
       COMPOSE=$(find /home -name "docker-compose.yaml" 2>/dev/null | rev | cut -c 20- | rev | tail -n 1)
     fi
     cd "$COMPOSE" || exit
+    # Docker-Compose v1
+    if [[ $DOCKER_COMPOSE_V1 == true ]]; then
+      echo -e "\n*** Updating Docker-Compose v1 (oldstable) ***\n"
+#      /usr/local/bin/docker-compose pull
+      /usr/local/bin/docker-compose up -d #--force-recreate --build
+    fi
+    # Docker-Compose v2
+    if [[ $DOCKER_COMPOSE_V2 == true ]]; then
+      echo -e "\n*** Updating Docker Compose ***"
+#      docker compose pull
+      docker compose up -d
+    fi
+    # Cleaning
+    echo -e "\n*** Cleaning ***"
+    docker container prune -f
+    docker system prune -a -f
+    docker image prune -f
+    docker system prune --volumes -f
   fi
-  # Docker-Compose v1
-  if [[ $DOCKER_COMPOSE_V1 == true ]]; then
-    echo -e "\n*** Updating Docker-Compose v1 (oldstable) ***\n"
-    /usr/local/bin/docker-compose pull
-    /usr/local/bin/docker-compose up --force-recreate --build -d
-  fi
-  # Docker-Compose v2
-  if [[ $DOCKER_COMPOSE_V2 == true ]]; then
-    echo -e "\n*** Updating Docker Compose ***"
-    docker compose pull
-    docker compose up -d
-  fi
-  # Cleaning
-  echo -e "\n*** Cleaning ***"
-  docker container prune -f
-  docker system prune -a -f
-  docker image prune -f
-  docker system prune --volumes -f
 fi
