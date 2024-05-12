9bdbc0e640d74b2897bcb963c9ac6f72852d96cd
BassT <30832786+BassT23@users.noreply.github.com>
2024-05-12 19:13:57 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -690,7 +690,7 @@ UPDATE_VM () {
     else
       echo -e "${BL}[Info]${GN} Try to connect via SSH${CL}"
       echo -e "${OR}This will take some time, please wait${CL}"
-      echo -e "${OR}!!! During development, sleep time could be set in config !!!${CL}"
+      echo -e "${OR}!!! During development, sleep $VM_START_DELAY secounds - could be set in config !!!${CL}"
       sleep "$VM_START_DELAY"
       SSH_CONNECTION=true
       OS_BASE=$(qm config "$VM" | grep ostype)
@@ -752,7 +752,7 @@ UPDATE_VM () {
 # QEMU
 UPDATE_VM_QEMU () {
   echo -e "${BL}[Info]${GN} Try to connect via QEMU${CL}"
-  echo -e "${OR}This will take some time, ... time could be set in config!\n${CL}"
+  echo -e "${OR}$VM_START_DELAY secounds wait time - could be set in config\n${CL}"
   sleep "$VM_START_DELAY"
   if qm guest exec "$VM" test >/dev/null 2>&1; then
     echo -e "${OR}  QEMU found. SSH connection is also available - with better output.${CL}\n\
