ed6733baf6ad40386573d0c21915b267c4f15ead
BassT23 <github@familiebecke.de>
2025-08-22 23:39:37 +0200

    fix #188 - hopefully

--- update-extras.sh
+++ update-extras.sh
@@ -132,7 +132,7 @@ if [[ $DOCKER_COMPOSE_V1 == true || $DOCKER_COMPOSE_V2 == true ]] && [[ $DOCKER_
 fi
 
 # Community / Helper Scripts
-  #188
+apt install expect
 if grep -q "community-scripts" /usr/bin/update 2>/dev/null && [[ $INCLUDE_HELPER_SCRIPTS == true ]];then
   stdbuf -oL -eL expect <<EOF | grep -v "whiptail"
 set timeout 3
