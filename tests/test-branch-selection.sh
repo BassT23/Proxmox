a3556d0912b210a06c0cb27696740902f8bb7a12
BassT <github@familiebecke.de>
2026-08-17 13:01:34 +0200

    test: cover persisted beta self-update branch

--- tests/test-branch-selection.sh
+++ tests/test-branch-selection.sh
@@ -12,7 +12,7 @@ grep -Fq 'FETCH_REMOTE_VERSION beta update.sh' "$ROOT_DIR/tag-filter.sh"
 grep -Fq 'BETA_VERSION=' "$ROOT_DIR/tag-filter.sh"
 grep -Fq 'master|beta|develop)' "$ROOT_DIR/install.sh"
 grep -Fq 'tarball/' "$ROOT_DIR/install.sh"
-grep -Fq 'BRANCH="${BRANCH:-master}"' "$ROOT_DIR/update.sh"
+grep -Fq "BRANCH=\"\${BRANCH:-master}\"" "$ROOT_DIR/update.sh"
 if grep -Fq 'beta branch is no longer active' "$ROOT_DIR/update.sh" "$ROOT_DIR/welcome-screen.sh"; then
   exit 1
 fi
